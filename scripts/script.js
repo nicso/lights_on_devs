2124686317edc161773f190f5d426b3f19aa5dfd
nicso <le.nicso@gmail.com>
2024-09-10 14:12:34 +0200

    filter options ui

--- scripts/script.js
+++ scripts/script.js
@@ -22,10 +22,10 @@ class User{
 }
 
 //region fake Bd
-let user1 = new User("user 1", "https://loremflickr.com/50/50?random=1");
-let user2 = new User("user 2", "https://loremflickr.com/50/50?random=1");
-let user3 = new User("user 3", "https://loremflickr.com/50/50?random=1");
-let user4 = new User("user 4", "https://loremflickr.com/50/50?random=1");
+let user1 = new User("user 1", "https://loremflickr.com/50/50/cat?random=1");
+let user2 = new User("user 2", "https://loremflickr.com/50/50/cat?random=2");
+let user3 = new User("user 3", "https://loremflickr.com/50/50/cat?random=3");
+let user4 = new User("user 4", "https://loremflickr.com/50/50/cat?random=4");
 
 let item1 = new GalleryElement("Project 1", "https://loremflickr.com/450/270?random=1", ["tag1", "tag2", "tag3"], user1);
 let item2 = new GalleryElement("Project 2", "https://loremflickr.com/450/270?random=2", ["tag1", "tag2", "tag3"], user2);
@@ -49,13 +49,13 @@ function addSpotlightElement(element, i){
     title.textContent = element.title;
     userName.textContent = element.user.name;
     avatar.src = element.user.image;
-    image.src = "https://loremflickr.com/450/270/cat,dog?random=" + i;
+    image.src = element.image;
 
     for (let j = 0; j < element.tags.length; j++) {
         let t = document.createElement("a");
         t.textContent = element.tags[j];
         t.classList.add("tag");
-        t.href = "?tag=" + "tag";
+        t.href = "?tag=" + element.tags[j];
         tags.append(t);
     }
 }
@@ -63,6 +63,41 @@ function addSpotlightElement(element, i){
 const gallery = document.querySelector(".mini-grid");
 let galleryElems = [item1,item2,item3,item4];
 
-for (let i = 0; i < galleryElems.length; i++) {
+function addGalleryElement(elem) {
+    let div = document.createElement("div");
+    div.classList.add('grid-elem');
+    gallery.prepend(div);
+
+    let image = document.createElement("img");
+    image.src = elem.image;
+    image.classList.add('thumb');
+    div.append(image);
     
+    let title = document.createElement("h2");
+    title.textContent = elem.title;
+    div.append(title);
+
+    let userName = document.createElement("h3");
+    userName.textContent = elem.user.name;
+    div.append(userName);
+
+    let avatar = document.createElement("img");
+    avatar.src = elem.user.image;
+    avatar.classList.add("avatar");
+    div.append(avatar);
+
+    let tags = document.createElement("div");
+    tags.classList.add("tags");
+    for (let j = 0; j < elem.tags.length; j++) {
+        let t = document.createElement("a");
+        t.textContent = elem.tags[j];
+        t.classList.add("tag");
+        t.href = "?tag=" + elem.tags[j];
+        tags.append(t);
+    }
+    div.append(tags);
+}
+
+for (let i = 0; i < galleryElems.length; i++) {
+    addGalleryElement(galleryElems[i]);
 }
